28678a37340bec1642862edfdfa916da7b7de833
RenataNovais <rnovaiscs@gmail.com>
2020-06-16 15:36:15 -0300

    Content exercises of day 8.1 - array functions

---
+++ front-end/block-08/trybe-8.1/some-every.js
@@ -0,0 +1,44 @@
+// SOME
+// Testando valores de elementos de um array
+function isBiggerThan10(element, index, array) {
+  return element > 10;
+}
+[2, 5, 8, 1, 4].some(isBiggerThan10);  // false
+[12, 5, 8, 1, 4].some(isBiggerThan10); // true
+// reescrevendo em arrow function
+[2, 5, 8, 1, 4].some(elem => elem > 10);  // false
+[12, 5, 8, 1, 4].some(elem => elem > 10); // true
+
+// verifica se possui algum nome que começa com a letra desejada
+const listNames = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+const verifyFirstLetter = (letter, names) => {
+  return names.some((name) => name[0] === letter);
+};
+
+console.log(verifyFirstLetter('J', listNames)); // true
+console.log(verifyFirstLetter('x', listNames)); // false
+
+// EVERY
+// Testando tamanho de todos os elementos do vetor
+function isBigEnough(element, index, array) {
+  return element >= 10;
+}
+[12, 5, 8, 130, 44].every(isBigEnough);   // false
+[12, 54, 18, 130, 44].every(isBigEnough); // true
+// reescrevendo com arrow function
+[12, 5, 8, 130, 44].every(elem => elem >= 10); // false
+[12, 54, 18, 130, 44].every(elem => elem >= 10); // true
+
+// verificando se o estudante foi aprovado em todas as materias
+const notes = {
+  portugues: 'Aprovado',
+  matematica: 'Reprovado',
+  ingles: 'Aprovado',
+};
+
+const verifyNotes = (studentNotes) => {
+  return Object.values(studentNotes).every((note) => note === 'Aprovado');
+};
+
+console.log(verifyNotes(notes));
