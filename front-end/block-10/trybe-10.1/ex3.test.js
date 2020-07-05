b864f370af0f710d246fa61b9ec182411892dd0e
RenataNovais <rnovaiscs@gmail.com>
2020-07-05 15:45:53 -0300

    Adding exercises 3 - test jest async with async/await

--- front-end/block-10/trybe-10.1/ex3.test.js
+++ front-end/block-10/trybe-10.1/ex3.test.js
@@ -1,18 +1,39 @@
 const users = {
-  : { name: 'Mark' },
-  : { name: 'Paul' }
-  };
+  4: { name: 'Mark' },
+  5: { name: 'Paul' }
+};
   
-  const findUserById = (id) => {
-    return new Promise((resolve, reject) => {
-        if (users[id]) {
-          return resolve(users[id]);
-        }
-  
-        return reject({ error: 'User with ' + id + ' not found.' });
+const findUserById = (id) => {
+  return new Promise((resolve, reject) => {
+      if (users[id]) {
+        return resolve(users[id]);
+      }
+
+      return reject({ error: 'User with ' + id + ' not found.' });
+  });
+}
+
+const getUserName = (userID) => {
+  return findUserById(userID).then(user => user.name);
+}
+
+describe('testing with async/await', () => {
+  describe('when the user id exists', () => {
+    it('if the promises succeed', async () => {
+      expect.assertions(1);
+      const user = await getUserName(4);
+      expect(user).toEqual('Mark');
     });
-  }
-  
-  const getUserName = (userID) => {
-    return findUserById(userID).then(user => user.name);
-  }
+  });
+
+  describe('when the user id does not exists', () => {
+    it('if the promises fails', async () => {
+      expect.assertions(1);
+      try {
+        await getUserName(2);
+      } catch (error) {
+        expect(error).toEqual({ error: 'User with 2 not found.' });
+      }
+    });
+  });
+});
