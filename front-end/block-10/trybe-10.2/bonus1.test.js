8f224b28813b01c4f8999968977d583563a3c7d7
RenataNovais <rnovaiscs@gmail.com>
2020-07-06 22:56:44 -0300

    exercise bonus - day 10.2

---
+++ front-end/block-10/trybe-10.2/bonus1.test.js
@@ -0,0 +1,33 @@
+// O código abaixo utiliza uma API de piadas e implementa o fetchJoke, que é um gerador de piadas ruins.
+// As piadas são geradas aleatoriamente através do endpoint armazenado em API_URL.
+// Faça um teste que verifique a chamada dessa API para um resultado específico.
+// Para isso, faça um mock do fetch, que faz a chamada à API, utilizando os seguintes dados:
+
+// {
+//   'id': '7h3oGtrOfxc',
+//   'joke': 'Whiteboards ... are remarkable.',
+//   'status': 200
+// }
+
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+  return fetch(API_URL, { headers: { Accept: "application/json" } })
+    .then(response => response.json())
+    .then(data => data.joke);
+};
+
+const fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () =>
+      Promise.resolve({
+        id: "7h3oGtrOfxc",
+        joke: "Whiteboards ... are remarkable.",
+        status: 200
+      })
+  })
+);
+
+test('fetch needs to return a joke', () => {
+  expect(fetchJoke()).resolves.toBe('Whiteboards ... are remarkable.');
+});
