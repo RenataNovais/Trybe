cea1c2b83fa6de3d98bea73e0dfd2ac700924ff2
RenataNovais <rnovaiscs@gmail.com>
2020-10-03 11:58:35 -0300

    Updates

---
+++ precache-manifest.7942545c4561d94542bd6d8c3705d6c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd195a773341e62931a6caab53813a8c",
+    "url": "/movie-library-stateful/index.html"
+  },
+  {
+    "revision": "02b026b96388b5a3bf6f",
+    "url": "/movie-library-stateful/static/css/main.9d45c01e.chunk.css"
+  },
+  {
+    "revision": "212cdba34f355b1652cf",
+    "url": "/movie-library-stateful/static/js/2.a924ad4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-library-stateful/static/js/2.a924ad4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02b026b96388b5a3bf6f",
+    "url": "/movie-library-stateful/static/js/main.7ced8bef.chunk.js"
+  },
+  {
+    "revision": "2dd7d8f57975cb0b3cdc",
+    "url": "/movie-library-stateful/static/js/runtime-main.38ed7d01.js"
+  }
+]);
